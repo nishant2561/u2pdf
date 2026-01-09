5d253006d5a7e4a7c96df0937c8eda32fb9da282
NISHANT KUMAR GUPTA <nishant2511.pahi@gmail.com>
2026-01-10 03:19:02 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/u2pdf/',
+  base: '/',
+,
   build: {
     outDir: 'dist',
     emptyOutDir: true,

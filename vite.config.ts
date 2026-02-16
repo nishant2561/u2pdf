ea1afca06c225c7e63504d19b32a62136556c4bf
NISHANT KUMAR GUPTA <nishant2511.pahi@gmail.com>
2026-02-16 16:49:49 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/u2pdf/',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
